bc9b63e586718ac9d1b3660a0288b52f4c9938bf
jzhou-k <jiayizhou22918@gmail.com>
2024-11-16 16:20:58 -0500

    fix client

---
+++ client/src/App.js
@@ -0,0 +1,84 @@
+// // import React from "react";
+// import logo from "./logo.svg";
+// // import "./App.css";
+// // import { useSpeech } from "react-text-to-speech";
+// // // import { LobeTTS } from '@lobehub/tts';
+
+// // function App() {
+// //   const [data, setData] = React.useState(null);
+
+// //   React.useEffect(() => {
+// //     fetch("/api")
+// //       .then((res) => res.json())
+// //       .then((data) => setData(data.message));
+// //   }, []);
+
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>{!data ? "Loading..." : data}</p>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+
+
+// import React from "react";
+// import { useSpeech } from "react-text-to-speech";
+
+// export default function App() {
+//   const text = `fuck you`;
+//   const { Text, speechStatus, start, pause, stop } = useSpeech({ text, pitch: 1, rate: 1, volume: 1, lang: "", voiceURI: "", autoPlay: false, highlightText: false });
+
+//   return (
+//     <div style={{ margin: "1rem", whiteSpace: "pre-wrap" }}>
+//       <div style={{ display: "flex", columnGap: "1rem", marginBottom: "1rem" }}>
+//         <button disabled={speechStatus === "started"} onClick={start}>
+//           Start
+//         </button>
+//         <button disabled={speechStatus === "paused"} onClick={pause}>
+//           Pause
+//         </button>
+//         <button disabled={speechStatus === "stopped"} onClick={stop}>
+//           Stop
+//         </button>
+//       </div>
+//       <Text />
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//          <p>{"Loading..."}</p>
+//        </header>
+//     </div>
+//     //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>{!data ? "Loading..." : data}</p>
+// //       </header>
+// //     </div>
+//   );
+// }
+
+
+
+
+
+import './App.css';
+import Speech from './Speech';
+import Dictaphone from './Dictaphone.js';
+function App() {
+
+
+    return (
+        <div className="App">
+            
+            <Dictaphone />
+            <Speech />
+        </div>
+
+    );
+}
+export default App;
